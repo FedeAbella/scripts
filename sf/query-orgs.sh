0ce4dd643be1489a05625a8e205e2c5f14162e3c
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-06-12 20:55:51 -0300

    clean up query orgs

--- sf/query-orgs.sh
+++ sf/query-orgs.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-if [[ ! -d "$HOME"/.config/sf-orgs ]]; then
-    mkdir "$HOME"/.config/sf-orgs
-fi
+declare -A types=(
+    ["Other"]="#cba6f7"
+    ["Sandbox"]="#f9e2af"
+    ["DevHub"]="#a6e3a1"
+    ["Scratch"]="#cdd6f4"
+)
+
+format() {
+    read -r org
+    for type in "${!types[@]}"; do
+        org=$(sed "s|\"\/${type}\/|<span color='${types["$type"]}'> ($type) |" <(echo "$org"))
+    done
+    org=$(sed 's|"|</span>|' <(echo "$org"))
+    echo "$org"
+}
+
+command -v sf >/dev/null || exit 1
 
 orgs=$(sf org list --skip-connection-status --json)
-if [[ ! $? ]]; then
-    exit 1
-fi
+[ $? ] || exit 1
 
-if [[ ! $(grep '"status": [0-9]' <(echo "$orgs") | sed 's/.*: \([0-9]\)\+.*/\1/') ]]; then
-    exit 1
-fi
+[ "$(grep '"status": [0-9]' <(echo "$orgs") | sed 's/.*: \([0-9]\)\+.*/\1/')" ] || exit 1
 
-logins=$(jq '[.result.nonScratchOrgs.[] | {username, alias, isDevHub: .isDevHub // false, isSandbox: .isSandbox // false, isScratch: false}] + [.result.scratchOrgs.[] | {username, alias, isDevHub, isSandbox, isScratch}]' <(echo "$orgs"))
+chosen=$(jq '[.result.nonScratchOrgs.[] | {username, alias, isDevHub: .isDevHub // false, isSandbox: .isSandbox // false, isScratch: false}] + [.result.scratchOrgs.[] | {username, alias, isDevHub, isSandbox, isScratch}] | .[] | "/\(if .isScratch then "Scratch" elif .isDevHub then "DevHub" elif .isSandbox then "Sandbox" else "Other" end)/\(.alias)"' \
+    <(echo "$orgs") |
+    format |
+    sort |
+    rofi -dmenu -markup-rows -p "  Orgs" -format p | sed 's/(.*) //')
 
-chosen=$(jq '.[].alias' <(echo "$logins") | sed 's/"/<b>/' | sed 's/"/<\/b>/' | sort | rofi -dmenu -markup-rows -p "  Orgs" -format p)
-if [[ -z "$chosen" ]]; then
-    exit 1
-fi
+[ -n "$chosen" ] || exit 1
+
 sf org open -o "$chosen"
