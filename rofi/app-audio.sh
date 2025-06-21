8606b2275f8f52ff6842c231ea05599917b4be9d
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-06-21 00:27:54 -0300

    make app audio switcher larger

--- rofi/app-audio.sh
+++ rofi/app-audio.sh
@@ -4,9 +4,9 @@
 sinks=$(pactl -f json list sinks 2>/dev/null | jq '[.[] | {name: .properties."alsa.card_name", id: .index}]')
 apps=$(pactl -f json list sink-inputs | jq '[.[] | {name: .properties."application.name", id: .index, media: .properties."media.name"}]')
 
-chosen_app=$(jq '.[] | "\(.name) [\(.media)]"' <(echo "$apps") | sed -e 's/\(^"\)\|\("$\)//g' | rofi -dmenu -p "Choose app:" -format i)
+chosen_app=$(jq '.[] | "\(.name)    --    \(.media)"' <(echo "$apps") | sed -e 's/\(^"\)\|\("$\)//g' | rofi -theme-str "*{ width: 1000;}" -dmenu -p "Choose app:" -format i)
 [[ -n "$chosen_app" ]] || exit 0
-chosen_sink=$(jq '.[] | .name' <(echo "$sinks") | rofi -dmenu -p "Choose output:" -format i)
+chosen_sink=$(jq '.[] | .name' <(echo "$sinks") | sed -e 's/\(^"\)\|\("$\)//g' | rofi -dmenu -p "Choose output:" -format i)
 [[ -n "$chosen_sink" ]] || exit 0
 
 pactl move-sink-input "$(echo "$apps" | jq --argjson ind "$chosen_app" '.[$ind] | .id')" "$(echo "$sinks" | jq --argjson ind "$chosen_sink" '.[$ind] | .id')"
