4ec770c989669f60145e75169d203a857aed92d5
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-07-24 09:25:26 -0300

    stagger startup apps

--- hypr/startup-apps.sh
+++ hypr/startup-apps.sh
@@ -8,18 +8,21 @@ case $user in
         hyprctl dispatch exec "[workspace 4] kitty -e btop"
         hyprctl dispatch exec "[workspace 7 silent] flatpak run com.rtosta.zapzap"
         hyprctl dispatch exec "[workspace 7 silent] discord"
-        steam -silent &
-        sleep 3
+        sleep 5
         hyprctl dispatch exec "[workspace 1] firefox"
+        sleep 60
+        steam -silent &
         ;;
     attentis)
         hyprctl dispatch exec "[workspace 7 silent] teams-for-linux --enable-features=UseOzonePlatform --ozone-platform=wayland"
         hyprctl dispatch exec "[workspace 4 silent] chromium --profile-directory='Profile 1'"
-        hyprctl dispatch exec "[workspace 2 silent] kitty"
-        hyprctl dispatch exec "[workspace 9 silent] firefox"
+        sleep 5
         hyprctl dispatch exec "[workspace 7 silent] flatpak run com.rtosta.zapzap"
-        sleep 3
+        sleep 5
+        hyprctl dispatch exec "[workspace 2 silent] kitty"
         hyprctl dispatch exec "[workspace 1] chromium --profile-directory='Default'"
+        sleep 60
+        hyprctl dispatch exec "[workspace 9 silent] firefox"
         ;;
     *) ;;
 esac
