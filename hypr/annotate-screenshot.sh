2a53a41c8b1470b3016c88105fd6a9d316d58421
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-07-29 22:22:00 -0300

    change to hyprcap

--- hypr/annotate-screenshot.sh
+++ hypr/annotate-screenshot.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 ## Take a screenshot with hyprshot and annotate with satty
 
-filename=$(hyprshot --mode region --freeze -- echo)
-satty --filename "$filename" --output-filename "$(dirname "$filename")/$(basename "$filename" .png)"_annotated.png
+filename=$(hyprcap shot region --freeze --no-notify -F 2>/dev/null | sed -e 's/[^\/]*\(\/\)/\1/')
+output_filename=$(date +'%F-%H%M%S_annotated.png')
+wait_deadline=$((SECONDS + 5))
+if [[ -n "$filename" ]]; then
+    while [[ ! -f "$filename" ]] && [[ $SECONDS -lt $wait_deadline ]]; do
+        :
+    done
+    satty --filename "$filename" --output-filename "$HOME/Pictures/Screenshots/$output_filename"
+else
+    notify-send -e "hyprcap failed"
+fi
